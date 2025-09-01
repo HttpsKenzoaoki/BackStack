b2a7398415e406bcd4d3c6ec8396ab9312cbebe4
WILIAM KENZO AOKI DA SILVA <wiliam.silva@etec.sp.gov.br>
2025-09-01 17:30:52 -0300

    Adição de arquivo

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+});
+
+app.get('/usuarios', (req, res) => {
+  res.send('Ok, deu bom')
+});
+
+app.listen(3000)
+
+
